177c08a0d42ee19f76627dad602e20ecdf18e4eb
Inge <inge.becht91@gmail.com>
2012-05-05 21:42:21 +0200

    added build map

---
+++ build/CMakeFiles/game.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/inge/Dropbox/c++/simple_game/character.cpp" "/home/inge/Dropbox/c++/simple_game/build/CMakeFiles/game.dir/character.cpp.o"
+  "/home/inge/Dropbox/c++/simple_game/global.cpp" "/home/inge/Dropbox/c++/simple_game/build/CMakeFiles/game.dir/global.cpp.o"
+  "/home/inge/Dropbox/c++/simple_game/object.cpp" "/home/inge/Dropbox/c++/simple_game/build/CMakeFiles/game.dir/object.cpp.o"
+  "/home/inge/Dropbox/c++/simple_game/test.cpp" "/home/inge/Dropbox/c++/simple_game/build/CMakeFiles/game.dir/test.cpp.o"
+  "/home/inge/Dropbox/c++/simple_game/timer.cpp" "/home/inge/Dropbox/c++/simple_game/build/CMakeFiles/game.dir/timer.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/game.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/game.dir/test.cpp.o"
+  "CMakeFiles/game.dir/character.cpp.o"
+  "CMakeFiles/game.dir/timer.cpp.o"
+  "CMakeFiles/game.dir/object.cpp.o"
+  "CMakeFiles/game.dir/global.cpp.o"
+  "game.pdb"
+  "game"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
